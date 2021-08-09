1477a885ea5dd8b92c4161dc5253a4134aaf2dbc
hannahk0108 <hannahk0108@gmail.com>
2021-08-09 00:53:39 -0500

    expense app done but there is no backend yet

---
+++ src/context/AppReducer.js
@@ -0,0 +1,21 @@
+// reducer, how we specify the application state changes in response to certain actaions to our store, our context
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+    default:
+      return state;
+  }
+};
+
+export default AppReducer;
